691bd8ae42190455aca938cd5d7dcd17109c2c36
Pooja Sahu <114080807+pooja3651@users.noreply.github.com>
2025-03-09 23:33:03 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -5,22 +5,10 @@
 def add(a, b):
     return a + b
 
-
-
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
 
 
 
-
-
-
-
-
-#if __name__ == "__main__":
-#    num1 = 5
- #   num2 = 10
-  #  result = add(num1, num2)
-   # print(f"The sum of {num1} and {num2} is {result}")
 
