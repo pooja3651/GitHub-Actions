9569514c1abe334bf049f4dd6c31b86dc35d484c
pooja3651 <your-email@example.com>
2025-03-09 17:18:07 +0000

    Added addition script and my-second-actions workflow in GitHub Actions workflow

---
+++ src/addition.py
@@ -0,0 +1,26 @@
+# app.py
+# This is a test commit
+
+
+def add(a, b):
+    return a + b
+
+
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
+
+
+
+
+
+
+
+
+#if __name__ == "__main__":
+#    num1 = 5
+ #   num2 = 10
+  #  result = add(num1, num2)
+   # print(f"The sum of {num1} and {num2} is {result}")
+
